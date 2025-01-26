9ec4cfea1bff755d9681c9b977ad19e84a4b7317
wahaj2 <wahajk901@gmail.com>
2025-01-27 00:20:23 +0500

    Updated app.py with new features/fixes

--- app.py
+++ app.py
@@ -54,14 +54,37 @@ from scipy.stats import skew, kurtosis, norm ,gaussian_kde
 
 
 
-data  = pd.read_csv('data.csv')
+#Loading data
+#importing the data table
+tables = pd.read_html("https://www.feanalytics.com/Table.aspx?SavedResultsID=e33df1a2-7b9a-ee11-b204-002248818b97&xl=1&UserID=B2E540A8-D474-4C56-9088-1F155612F7FE&xlRefreshData=1")
+#Saving the data into Dateframe
+data = tables[0]
+#Taking the transpose of table
+data = data.T
+#Spliting the dates from string format 
+data[1][1][26:36]
+#Looping into column and saving the dates in list 
+dates = []
+for i in range (1,71):
+    date = data[1][i][26:36]
+    dates.append(date)
+#Droping the first and second columns
+data = data.drop(columns=[0])
+data = data.drop(columns=[1])
+#Naming the columns 
+data.columns = data.iloc[0]
+data = data.iloc[1:].reset_index(drop=True)
+#Now droping the Null values rows 
+data.dropna(inplace = True)
+#Adding the dates 
+data['Dates'] = dates
+#Converting the date into pandas datetime format
 data['Dates'] = pd.to_datetime(data.Dates)
 #Setting the Dates as index in our table
 data.set_index('Dates', inplace= True)
 #Changing the column data into float values
 for i in range(len(data.columns)):
     data[data.columns[i]]=data[data.columns[i]].astype(float)
-data
 
 
 
@@ -3647,7 +3670,7 @@ def sharp_optimize_button_click(n_clicks, data_table_data, minimum_weight, maxim
    
 
 if __name__ == '__main__':
-    app.run_server(debug=False,host="0.0.0.0",port=80)
+    app.run_server(debug=True)
 
 
 # In[ ]:
